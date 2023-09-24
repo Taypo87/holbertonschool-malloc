ccfde2ccba726f3b5c834f3db39172b2ba0664ce
Taypo87 <taylor.poczynek87@gmail.com>
2023-09-24 20:41:34 +0000

    trying to find the segfault

--- malloc.c
+++ malloc.c
@@ -10,31 +10,29 @@ void *_malloc(size_t size)
 	void *ptr;
 	static void *heap_start, *chunk_start;
 	static size_t unused;
-	size_t *size_ptr;
+	size_t size_ptr;
 
 	if (heap_start == NULL)
 		heap_start = sbrk(0);
-	if (!unused	|| unused < size)
-	{
-		unused += sysconf(_SC_PAGESIZE);
-		if (!chunk_start)
-			chunk_start = sbrk(sysconf(_SC_PAGESIZE));
-		else
-			sbrk(sysconf(_SC_PAGESIZE));
-	}
-	while (unused < size)
+	if (!unused)
+		unused = sysconf(_SC_PAGESIZE);
+	if (!chunk_start)
+		chunk_start = heap_start;
+
+	while (unused < (size + sizeof(size_t)))
 	{
 		sbrk(sysconf(_SC_PAGESIZE));
 		unused += sysconf(_SC_PAGESIZE);
 	}
-	if (size % 8 != 0)
-		size += 8 - (size % 8);
-
+	while (size % 8 != 0)
+	{
+		size++;
+	}
 	ptr = chunk_start;
 	chunk_start = (char *)chunk_start + size + sizeof(size_t);
 	unused -= size;
-	size_ptr = (size_t *)ptr;
-    *size_ptr = size;
+	size_ptr = (size_t)ptr;
+    size_ptr = size;
 	ptr = (char *)ptr + sizeof(size_t);
 
 	return (ptr);
